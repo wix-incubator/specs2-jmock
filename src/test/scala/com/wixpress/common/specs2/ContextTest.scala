7cfa6b8b0f3ae206f2665ff46a9185850ee5657e
Nimrod Argov <nimroda@wix.com>
2015-04-29 09:13:10 +0300

    added test for multiple contexts

---
+++ src/test/scala/com/wixpress/common/specs2/ContextTest.scala
@@ -0,0 +1,39 @@
+package com.wixpress.common.specs2
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+class ContextTest extends Specification with JMock{
+
+  trait Context extends Scope {
+    val mockFoo = mock[Foo]
+  }
+
+  "A bar" should {
+    "invoke f on foo" in new Context {
+      checking {
+        ignoring(mockFoo).g()
+        oneOf(mockFoo).f()
+      }
+
+      new Bar(mockFoo)
+    }
+    "invoke g on foo" in new Context {
+      checking {
+        allowing(mockFoo).f()
+        oneOf(mockFoo).g()
+      }
+
+      new Bar(mockFoo)
+    }
+  }
+}
+
+trait Foo {
+  def f()
+  def g()
+}
+class Bar(foo: Foo){
+  foo.f()
+  foo.g()
+}
